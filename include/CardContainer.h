521a22fd659a1c3251990efc923efef47eef5edd
sci10n <martin.estgren@gmail.com>
2014-11-05 23:56:29 +0100

    Added CardContainerTest and fixed CardContainer.cc

--- include/CardContainer.h
+++ include/CardContainer.h
@@ -1,4 +1,8 @@
 //Athor Martin
+//============
+//Default constructor deleted because it cant set the _id field any other way.
+//Allows copy but not moving because a CardContainer will always be present att same memory addr 
+//but allowd clones for BoardState.
 #ifndef CARDCONTAINER_H
 #define CARDCONTAINER_H
 #include <vector>
@@ -7,10 +11,10 @@
 class CardContainer
 {
 private:
-	std::vector<CardID> _cards;
+	std::vector<CardID*> _cards;
 	ContainerID* _id;
 public:
-	CardContainer(std::vector<CardID> cards, ContainerID* id):_cards{cards}, _id{id}
+	CardContainer(std::vector<CardID*> cards, ContainerID* id):_cards{cards}, _id{id}
 	{}
 	CardContainer(ContainerID* id): _id{id}
 	{}
@@ -26,10 +30,13 @@ public:
 	//Default Destructor
 	~CardContainer() = default;
 
-	void addCard(const CardID *);
-	void removeCard(const CardID *);
+	//public Modifiers
+	void addCard(CardID*);
+	void removeCard(CardID*);
+
+	//public Capacity
 	int getSize();
-	std::vector<CardID> getCards();
+	std::vector<CardID*> getCards();
 	ContainerID* getID();
 };
 #endif
