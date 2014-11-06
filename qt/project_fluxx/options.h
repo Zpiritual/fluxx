d96de7f3bb979d0909e85ba5706b06c1c4c9ed51
Krewli <Krewli@hotmail.com>
2014-11-06 14:09:27 +0100

    Junk

---
+++ qt/project_fluxx/options.h
@@ -0,0 +1,18 @@
+#ifndef OPTIONS_H
+#define OPTIONS_H
+
+#include <QWidget>
+
+class Options : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit Options(QWidget *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // OPTIONS_H
