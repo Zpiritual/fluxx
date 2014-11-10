9ac06d6d0eabef01938c27e38d09db4b0e7c5029
Krewli <Krewli@hotmail.com>
2014-11-10 16:01:35 +0100

    Modified options-widget to work with the other parts of the mainmenu and merged it together

---
+++ qt/project_fluxx/options.h
@@ -0,0 +1,46 @@
+#ifndef OPTIONS_H
+#define OPTIONS_H
+
+#include <QWidget>
+#include <QtWidgets>
+#include "mainmenu.h"
+
+class Options : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit Options(QWidget *parent = 0);
+
+private:
+    QVBoxLayout* main_layout;
+    QHBoxLayout* profile_layout;
+    QVBoxLayout* vertical_profile_layout;
+    QHBoxLayout* finish_layout;
+
+    QListWidget* user_profiles;
+    QLineEdit*  profile_text;
+
+    QPushButton* add_profile_button;
+    QPushButton* remove_profile_button;
+
+    QPushButton* ok_button;
+    QPushButton* cancel_button;
+
+    MainMenu* main_menu;
+
+    bool profileExist(const QString&) const;
+    void profileMessage(const QString&) const;
+    void connectSignals();
+    void uiElements();
+
+signals:
+
+public slots:
+    void addProfile();
+    void removeProfile();
+    void enableRemoveButton();
+    void ok();
+    void cancel();
+};
+
+#endif // OPTIONS_H
