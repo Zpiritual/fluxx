18bcb74709929cfed025557b8f512698dca8f556
Krewli <Krewli@hotmail.com>
2014-11-20 17:43:35 +0100

    Added functionality to plasyerlist and playerlistitem

--- qt/project_fluxx/playerlist.h
+++ qt/project_fluxx/playerlist.h
@@ -3,14 +3,22 @@
 
 #include <QWidget>
 #include "../../include/PlayerID.h"
+#include "../../include/BoardSnapshot.h"
+#include "playerlistitem.h"
 
 class PlayerList : public QWidget
 {
     Q_OBJECT
 public:
-    explicit PlayerList(QWidget *parent = 0);
+    explicit PlayerList(const std::vector<PlayerID>&, QWidget *parent = 0);
     ~PlayerList();
     const PlayerID pickPlayer() const;
+    void updatePlayers(const BoardSnapshot&);
+
+private:
+    std::vector<PlayerID> player_ids;
+    std::vector<PlayerListItem*> players;
+
 
 signals:
 
--- qt/project_fluxx/playerlistitem.cpp
+++ qt/project_fluxx/playerlistitem.cpp
@@ -1,6 +1,29 @@
 #include "playerlistitem.h"
 
-PlayerListItem::PlayerListItem(QWidget *parent) :
-    QWidget(parent)
+PlayerListItem::PlayerListItem(const PlayerID& name, QWidget *parent) :
+    player_name(name), QWidget(parent)
 {
+    vertical_layout = new QVBoxLayout;
+    name_label.setText(QString::fromStdString(player_name.val));
+    card_count.setText(QString::number(hand.size()));
+
+    keepers_id.push_back(CardID{1});
+    keepers_id.push_back(CardID{2});
+    keepers_id.push_back(CardID{3});
+
+    for(int i=0; i < keepers_id.size(); ++i)
+    {
+        keepers.push_back(new CardButton(keepers_id.at(i)));
+    }
+}
+
+int PlayerListItem::getHandCount() const
+{
+    return hand.size();
+}
+
+void PlayerListItem::updateCards(const std::vector<CardID>& hnd, const std::vector<CardID>& keeprs)
+{
+    hand = hnd;
+    keepers_id = keeprs;
 }
