e2a3473a54cd9f587a3f437ebf5e536131f518ba
sci10n <martin.estgren@gmail.com>
2014-11-22 00:54:05 +0100

    Removed TriggeredRule

--- include/RuleManager.h
+++ include/RuleManager.h
@@ -11,7 +11,12 @@ enum class Direction
 	CW,
 	CCW 
 };
-
+enum class RuleTrigger
+{
+	PRE_DRAW,
+	POST_DRA,
+	END_TURN
+};
 class RuleManager
 {
 public:
--- src/Card.cc
+++ src/Card.cc
@@ -1,13 +1,17 @@
 #include "Card.h"
 
 Card::Card(const CardID id, const string type, const string subtype, const string description, const vector<Effect*> effects)
-	: _id{id}, _type{type}, _subtype{subtype}, _description{description}, _effects{effects}
+	:_id{id},
+	 _type{type},
+	 _subtype{subtype},
+	 _description{description},
+	 _effects{effects}
 	{}
 Card::~Card()
- {
+	{
 		for (Effect* i : _effects)
 			delete i;
-}
+	}
 
 	const CardID 		  Card::getID() 			const	{ return _id; }
 	const string 		  Card::getType() 			const	{ return _type; }
