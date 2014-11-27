c6d47739708beb4b3ef1d229f9c7944a60f410ea
postinternetsyndrome <lord.bamse@gmail.com>
2014-11-27 11:56:58 +0100

    Starting on GameLoop_test

--- include/RuleManager.h
+++ include/RuleManager.h
@@ -39,12 +39,12 @@ public:
 	void clearRules();
 
 private:
-	int _hand_limmit 	= 0;
-	int _keeper_limit	= 0;
-	int _goal_limit		= 0;
+	int _hand_limmit 	= 82;
+	int _keeper_limit	= 82;
+	int _goal_limit		= 1;
 	int _inflation		= 0;
-	int _draw			= 0;
-	int _play 			= 0;
+	int _draw			= 1;
+	int _play 			= 1;
 	Direction _play_direction = Direction::CLOCKWISE;
 	std::vector<TriggeredRule*> _triggered_rules;
 };
--- test/GameLoop_test.cc
+++ test/GameLoop_test.cc
@@ -1,9 +1,14 @@
 #include <iostream>
 #include "GameLoop.h"
+#include "DeckParser.h"
 
 using namespace std;
 
 int main()
 {
+	DeckParser p;
+	GameLoop gl{p.deck_to_game("../docs/10keepers.fluxx"), 2};
+	//gl.run();
+
 	return 0;
 }
