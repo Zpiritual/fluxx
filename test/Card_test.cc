882bcd5f3944afbd0990601cdfc72d6bd51ee399
postinternetsyndrome <lord.bamse@gmail.com>
2014-12-01 16:50:39 +0100

    Added "name" member to Card

--- test/Card_test.cc
+++ test/Card_test.cc
@@ -11,18 +11,20 @@ int main()
 {
 	vector<Effect> e1;
 	e1.push_back(Effect{"test string"});
-	Card c1(CardID(000), "type1", "subtype1", "description1", e1);
+	Card c1(CardID(001), "name1", "type1", "subtype1", "description1", e1);
 
 	cout << "CardID: " 	  << c1.getID().val
 		 << "\nType: " 		  << c1.getType()
 		 << "\nSubtype: "	  << c1.getSubtype()
 		 << "\nDescription: " << c1.getDescription();
 
-	Card c2(CardID(001), "type2", "subtype1", "description2", e1);
-	Card c3(CardID(002), "type2", "subtype1", "description2", e1);
-	Card c4(CardID(003), "type2", "subtype1", "description2", e1);
-	Card c5(CardID(000), "type2", "subtype1", "description2", e1);
+	Card c2(CardID(002), "name2", "type2", "subtype3", "description2", e1);
+	Card c3(CardID(003), "name3", "type3", "subtype4", "description3", e1);
+	Card c4(CardID(004), "name4", "type4", "subtype4", "description4", e1);
 
+	Card c5(CardID(001), "name1", "type1", "subtype1", "description1", e1);
+
+	//Make sure the equality operators function.
 	assert(c1.getID() != c2.getID());
 	assert(c2.getID() != c3.getID());
 	assert(!(c3.getID() == c4.getID()));
